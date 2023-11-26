2398a86bd33ca772df15725ff260ca48b509c16f
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-11-26 20:42:31 +0100

    WEB: Fix Next.js zustand hydration error

---
+++ frontend/store/useStore.tsx
@@ -0,0 +1,21 @@
+/* https://docs.pmnd.rs/zustand/integrations/persisting-store-data#usage-in-next.js */
+import { useState, useEffect } from "react";
+
+export const useStore = <T, F>(
+  store: (callback: (state: T) => unknown) => unknown,
+  callback: (state: T) => F,
+) => {
+  const result = store(callback) as F;
+  const [data, setData] = useState<F>();
+
+  useEffect(() => {
+    setData(result);
+  }, [result]);
+
+  return data;
+};
+
+// useage:
+/*
+const bears = useStore(yourStore, (state) => state.bears)
+*/
