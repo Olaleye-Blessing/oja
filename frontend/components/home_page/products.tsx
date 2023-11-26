de91856aa3f2b8ee11fc4a129890306acba932c2
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-11-26 23:36:39 +0100

    WEB: Improve products' page UI

--- frontend/components/home_page/products.tsx
+++ frontend/components/home_page/products.tsx
@@ -2,6 +2,9 @@ import { IProduct } from "@/interfaces/product";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
+import toast from "react-hot-toast";
+import { ShoppingCart } from "lucide-react";
 
 interface ProductsProps {
   products: IProduct[];
@@ -13,8 +16,11 @@ const Products = ({ products }: ProductsProps) => {
       {products.map((product) => {
         return (
           <li key={product.id}>
-            <Link href={`/products/${product.id}`}>
-              <figure className="bg-blue-500 flex items-center justify-center overflow-hidden mb-3 rounded-md">
+            <Link
+              href={`/products/${product.id}`}
+              className="h-full flex flex-col rounded-md cardboard"
+            >
+              <figure className="bg-blue-500 flex items-center justify-center overflow-hidden mb-3 rounded-md rounded-b-none">
                 <Image
                   className="w-full h-[20rem] object-cover rounded-md"
                   src={product.image}
@@ -23,33 +29,49 @@ const Products = ({ products }: ProductsProps) => {
                   height={200}
                 />
               </figure>
-              <p className="font-semibold">{product.name}</p>
-              <p className="line-clamp-3 mb-2">{product.description}</p>
-              <div className="flex items-center justify-between">
-                <p className="flex items-center justify-center">
-                  <span className="text-2xl">$</span>
-                  <span>{product.price}</span>
-                </p>
-                <div>
-                  <p
-                    className={`text-sm px-3 pt-[0.125rem] pb-[0.18rem] mb-1 rounded-md bg-black bg-opacity-90 ${
-                      product.condition === "new"
-                        ? "text-green-500"
-                        : "text-red-300"
-                    }`}
-                  >
-                    {product.condition}
-                  </p>
-                  <p>
-                    <span
-                      className={`font-semibold ${
-                        product.stock_quantity < 4 ? " text-red-600" : ""
+              <div className="flex flex-col p-2 flex-grow">
+                <p className="font-semibold">{product.name}</p>
+                <p className="line-clamp-3 mb-8">{product.description}</p>
+                <div className="flex items-center justify-between mt-auto">
+                  <div className="flex items-center justify-between flex-wrap w-full">
+                    <p className="flex items-center justify-center">
+                      <span className="text-2xl">$</span>
+                      <span>{product.price}</span>
+                    </p>
+                    <p
+                      className={`text-sm px-3 pt-[0.125rem] pb-[0.18rem] mb-1 rounded-md bg-black bg-opacity-90 ${
+                        product.condition === "new"
+                          ? "text-green-500"
+                          : "text-red-300"
                       }`}
                     >
-                      {product.stock_quantity}
-                    </span>{" "}
-                    <span>left</span>
-                  </p>
+                      {product.condition}
+                    </p>
+                    <p>
+                      <span
+                        className={`font-semibold ${
+                          product.stock_quantity < 4 ? " text-red-600" : ""
+                        }`}
+                      >
+                        {product.stock_quantity}
+                      </span>{" "}
+                      <span>left</span>
+                    </p>
+                  </div>
+                  <Button
+                    variant={"outline"}
+                    size={"icon"}
+                    type="button"
+                    className="p-0 text-primary ml-4"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+
+                      toast.success("Added to cart");
+                    }}
+                  >
+                    <ShoppingCart size={20} />
+                  </Button>
                 </div>
               </div>
             </Link>
