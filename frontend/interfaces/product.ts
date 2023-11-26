7da7b5bd57b5e227c6e05cb6e747f1e14a0914e4
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-11-26 23:33:04 +0100

    WEB: Update Product types

--- frontend/interfaces/product.ts
+++ frontend/interfaces/product.ts
@@ -4,7 +4,13 @@ export interface IProduct {
   price: number;
   description: string;
   stock_quantity: number;
-  category: string;
+  category_id: number;
+  category: {
+    id: number;
+    name: string;
+    inserted_at: string;
+    updated_at: string;
+  };
   condition: string;
   image: string;
   created_at: string;
