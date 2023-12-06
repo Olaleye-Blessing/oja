d0a128e1725e47bf229759b7f7ea9bd9b9992b9b
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-12-06 09:50:04 +0100

    WEB: Improve footer UI

--- frontend/components/footer/index.tsx
+++ frontend/components/footer/index.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="mt-auto text-center px-4 py-2">
       <p>© {new Date().getFullYear()} Oja, Inc. All rights reserved.</p>
+      <p className="mt-0.5">
+        Built by{" "}
+        <a
+          href="http://https://github.com/Olaleye-Blessing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary font-semibold border-b border-primary"
+        >
+          Blessing Olaleye
+        </a>
+      </p>
     </footer>
   );
 };
