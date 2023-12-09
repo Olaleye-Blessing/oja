c2ece597a774f931a004707fa3f3d188dc28b11a
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-12-09 11:16:58 +0100

    WEB: Enable users to log out

--- frontend/components/navbar/profile.tsx
+++ frontend/components/navbar/profile.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from "next/navigation";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
-import { UserType } from "@/store/useAuth";
+import { UserType, useAuthStore } from "@/store/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,12 +12,34 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { API_URL } from "@/utils/constant";
+import { getError } from "@/utils/get-error";
 
 interface ProfileProps {
   user: UserType;
 }
 
 const Profile = ({ user }: ProfileProps) => {
+  const router = useRouter();
+  const logout = useAuthStore((state) => state.logout);
+
+  const handleLogout = async () => {
+    try {
+      toast.loading("Logging out...", {
+        id: "logout",
+        position: "top-right",
+      });
+      await axios.delete(`${API_URL}/auth/logout`, {
+        withCredentials: true,
+      });
+
+      logout();
+      router.refresh();
+    } catch (error) {
+      toast.error(getError(error), { id: "logout" });
+    }
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="uppercase flex items-center justify-center rounded-full w-8 h-8 border font-semibold">
@@ -28,7 +53,11 @@ const Profile = ({ user }: ProfileProps) => {
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <button className="flex items-center justify-start w-full">
+          <button
+            className="flex items-center justify-start w-full"
+            type="button"
+            onClick={handleLogout}
+          >
             <LogOut size={16} />
             <span className="ml-2">Logout</span>
           </button>
